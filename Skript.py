84a07ec5f90854e3b10da8b1af1679ba764d9449
Heschy-Software <82758794+heschy@users.noreply.github.com>
2021-06-21 18:22:16 +0200

    Update Skript.py

--- Skript.py
+++ Skript.py
@@ -1,13 +1,10 @@
 import bpy;
 
-rendertime_reducing = 200;
-#Init
 BlenderRenderCycles                            = bpy.data.scenes['Scene'].cycles;
 BlenderRenderEevee                             = bpy.data.scenes['Scene'].eevee;
 BlenderRenderConfig                            = bpy.data.scenes['Scene'].render;
 bpy.data.scenes['Scene'].use_nodes             = True;
 
-#General Render Settings
 BlenderRenderConfig.engine                     = 'CYCLES';
 BlenderRenderConfig.tile_x                     = 128;
 BlenderRenderConfig.use_lock_interface         = True;
@@ -17,9 +14,8 @@ BlenderRenderConfig.use_persistent_data        = True;
 BlenderRenderConfig.film_transparent           = True;
 BlenderRenderConfig.fps                        = 25;
 
-#Cycles Settings
-BlenderRenderCycles.samples                    = rendertime_reducing;
-BlenderRenderCycles.aa_samples                 = rendertime_reducing;
+BlenderRenderCycles.samples                    = 50;
+BlenderRenderCycles.aa_samples                 = 50;
 BlenderRenderCycles.device                     = 'GPU';
 BlenderRenderCycles.use_denoising              = True;
 BlenderRenderCycles.use_preview_denoising      = True;
@@ -34,9 +30,16 @@ BlenderRenderCycles.denoiser                   = 'OPENIMAGEDENOISE'
 BlenderRenderCycles.preview_denoiser           = 'OPENIMAGEDENOISE'
 BlenderRenderCycles.use_preview_denoising      = False;
 BlenderRenderCycles.feature_set                = 'SUPPORTED';
+BlenderRenderCycles.use_adaptive_sampling      = True;
+BlenderRenderCycles.adaptive_threshold         = 0.0;
+BlenderRenderCycles.adaptive_min_samples       = 0;
 
-#Eevee Settings
 BlenderRenderEevee.use_bloom                   = True;
 BlenderRenderEevee.use_ssr                     = True;
 BlenderRenderEevee.taa_render_samples          = 500;
 BlenderRenderEevee.taa_samples                 = 0;
+
+
+# Delete the '#' in the folowing 2 lines if you want to render a 4K-HD-Image:
+#bpy.data.scenes["Scene"].render.resolution_x = 3840
+#bpy.data.scenes["Scene"].render.resolution_y = 2160
